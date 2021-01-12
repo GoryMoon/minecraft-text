bb9e35b54465ec6d7eda2b24b062dc6a2c50fff1
GoryMoon <gurreja@gmail.com>
2021-01-12 22:21:24 +0100

    Changed jest file to typescript

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true
+};
+
+export default config;
