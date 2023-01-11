e6a02e05977b6d891bae6d07df954668de155a11
GoryMoon <gurreja@gmail.com>
2023-01-11 02:33:32 +0100

    Jest: treat .ts as esm files

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    collectCoverage: true
+    collectCoverage: true,
+    extensionsToTreatAsEsm: ['.ts']
 };
 
 export default config;
